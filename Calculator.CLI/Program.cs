89728aa27a2fef9d43f9c87cb58aa2f65f0d1faf
milleniumbug <milleniumbug@protonmail.com>
2022-01-16 21:29:00 +0100

    Add CLI

---
+++ Calculator.CLI/Program.cs
@@ -0,0 +1,30 @@
+﻿using Calculator;
+
+var evaluationContext = new EvaluationContext();
+var parser = new Parser();
+
+while (true)
+{
+    string? line = Console.ReadLine();
+    if (line == null)
+    {
+        break;
+    }
+
+    var lexer = new Lexer(new Reader(line));
+    var parseResult = parser.Parse(lexer);
+    if (parseResult.HasError)
+    {
+        Console.WriteLine($"Error: {parseResult.Error.Reason}");
+        continue;
+    }
+
+    var evaluationResult = parseResult.Value.Evaluate(evaluationContext);
+    if (evaluationResult.HasError)
+    {
+        Console.WriteLine($"Error: {evaluationResult.Error.Reason}");
+        continue;
+    }
+    
+    Console.WriteLine($"Result: {evaluationResult.Value}");
+}
